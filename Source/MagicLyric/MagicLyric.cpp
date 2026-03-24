27b86ccb61bcf0665235d5838fe710d01987dd17
fexfox <fexfox@vip.qq.com>
2026-03-25 02:33:34 +0800

    update ue project

---
+++ Source/MagicLyric/MagicLyric.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MagicLyric.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MagicLyric, "MagicLyric" );
